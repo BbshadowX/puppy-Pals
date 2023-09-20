28d26dc5e33d173571cbec1abfdf265343050955
Bryan Crawford <bryancrawford14@gmail.com>
2023-09-20 19:42:15 -0400

    rendered names

--- puppy-Pals/src/App.jsx
+++ puppy-Pals/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import puppyList from "./data";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppyList);
+
+  return (
+    <>
+      {puppies.map((puppy) => {
+        return <p>{puppy.name}</p>;
+      })}
+    </>
+  );
 }
 
 export default App;
